a1997e1a162bfd10272db8aae399b700cda26c96
sani kumar <sanikumar21@gmail.com>
2019-03-23 20:45:51 +0530

    Update sanimachine.h

--- sanimachine.h
+++ sanimachine.h
@@ -6,6 +6,9 @@ class sani
   int *sani_experience; 
   double recursive;
  pubic 
- int work_recursive(int *S,int data,int 
+ int work_recursive(int *S,int data,int vertical,int horizontal)
+}
+
+   
  
  
